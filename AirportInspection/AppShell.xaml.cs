c4f172b12e6333dbcc322f777ac5e670f29af32f
Adam Jones <66421205+adamj06@users.noreply.github.com>
2026-01-07 13:45:57 +0000

    Add project files.

---
+++ AirportInspection/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AirportInspection
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AirportInspection/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AirportInspection
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
